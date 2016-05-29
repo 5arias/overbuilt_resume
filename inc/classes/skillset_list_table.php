e39f7dcfceea65f52f98c249ebb8fba80412fd9c
Brody <fivearias@gmail.com>
2016-05-29 00:26:32 -0400

    Added Ajax Submit & Update
    Fixed display errors in table, added ajax table loading and updating.

--- inc/classes/skillset_list_table.php
+++ inc/classes/skillset_list_table.php
@@ -14,11 +14,22 @@
  *
  */
  
+ ob_start();
  
- // WP_List_Table is not loaded automatically so we need to load it in our application
+// WP_List_Table is not loaded automatically so we need to load it in our application
 if( ! class_exists( 'WP_List_Table' ) ) {
     require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
+
+
+/* Hide notices to avoid AJAX errors
+ * Sometimes the Class throws a notice about 'hook_suffix' being undefined,
+ * which breaks every AJAX call.
+ */
+error_reporting( ~E_NOTICE );
+
+
+
 
 class Skillset_List_Table extends WP_List_Table {
 	
@@ -67,7 +78,7 @@ class Skillset_List_Table extends WP_List_Table {
 	/**
 	 * Delete a skill.
 	 *
-	 * @param int $id skill ID
+	 * @param int $id skill id
 	 */
 	public static function delete_skill( $id ) {
 		global $wpdb;
@@ -128,7 +139,7 @@ class Skillset_List_Table extends WP_List_Table {
 	 */
 	function column_cb( $item ) {
 		return sprintf(
-			'<input type="checkbox" name="bulk-delete[]" value="%s" />', $item['ID']
+			'<input type="checkbox" name="bulk-delete[]" value="%s" />', $item['id']
 		);
 	}
 
@@ -147,7 +158,7 @@ class Skillset_List_Table extends WP_List_Table {
 		$title = '<strong>' . $item['name'] . '</strong>';
 
 		$actions = [
-			'delete' => sprintf( '<a href="?page=%s&action=%s&skill=%s&_wpnonce=%s">Delete</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['ID'] ), $delete_nonce )
+			'delete' => sprintf( '<a href="?page=%s&action=%s&skill=%s&_wpnonce=%s">Delete</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['id'] ), $delete_nonce )
 		];
 
 		return $title . $this->row_actions( $actions );
@@ -209,17 +220,31 @@ class Skillset_List_Table extends WP_List_Table {
 
 		/** Process bulk action */
 		$this->process_bulk_action();
-
+		
+		/**
+		 * REQUIRED. We also have to register our pagination options & calculations.
+		 */
 		$per_page     = $this->get_items_per_page( 'skills_per_page', 5 );
 		$current_page = $this->get_pagenum();
 		$total_items  = self::record_count();
 
 		$this->set_pagination_args( [
-			'total_items' => $total_items, //WE have to calculate the total number of items
-			'per_page'    => $per_page //WE have to determine how many items to show on a page
+			//WE have to calculate the total number of items
+			'total_items' => $total_items,
+			//WE have to determine how many items to show on a page
+			'per_page'    => $per_page,
+			//WE have to calculate the total number of pages
+			'total_pages'	=> ceil( $total_items / $per_page )
+			// Set ordering values if needed (useful for AJAX)
+			//'orderby'	=> ! empty( $_REQUEST['orderby'] ) && '' != $_REQUEST['orderby'] ? $_REQUEST['orderby'] : 'title',
+			//'order'		=> ! empty( $_REQUEST['order'] ) && '' != $_REQUEST['order'] ? $_REQUEST['order'] : 'asc'
 		] );
-
+		
+		/**
+		 * REQUIRED. Set data
+		 */
 		$this->items = self::get_skills( $per_page, $current_page );
+		
 	}
 
 	public function process_bulk_action() {
@@ -236,7 +261,7 @@ class Skillset_List_Table extends WP_List_Table {
 			else {
 				self::delete_skill( absint( $_GET['skill'] ) );
 
-				wp_redirect( esc_url( add_query_arg() ) );
+				wp_redirect( esc_url_raw( add_query_arg() ) );
 				exit;
 			}
 
@@ -249,17 +274,102 @@ class Skillset_List_Table extends WP_List_Table {
 
 			$delete_ids = esc_sql( $_POST['bulk-delete'] );
 
-			// loop over the array of record IDs and delete them
+			// loop over the array of record ids and delete them
 			foreach ( $delete_ids as $id ) {
 				self::delete_skill( $id );
 
 			}
 
-			wp_redirect( esc_url( add_query_arg() ) );
+			wp_redirect( esc_url_raw( add_query_arg() ) );
 			exit;
 		}
-	}  
+	}
 	
 	
+	
+	
+	/**
+	 * Display the table
+	 * Adds a Nonce field and calls parent's display method
+	 *
+	 * @since 3.1.0
+	 * @access public
+	 */
+	function display() {
+
+		wp_nonce_field( 'ajax-skillset-table-nonce', '_ajax_skill_table_nonce' );
+
+		echo '<input type="hidden" id="order" name="order" value="' . $this->_pagination_args['order'] . '" />';
+		echo '<input type="hidden" id="orderby" name="orderby" value="' . $this->_pagination_args['orderby'] . '" />';
+
+		parent::display();
+	}
+	
+
+	/**
+	 * Handle an incoming ajax request (called from admin-ajax.php)
+	 *
+	 * @since 3.1.0
+	 * @access public
+	 */
+	function ajax_response() {
+
+		check_ajax_referer( 'ajax-skillset-table-nonce', '_ajax_skill_table_nonce' );
+
+		$this->prepare_items();
+
+		extract( $this->_args );
+		extract( $this->_pagination_args, EXTR_SKIP );
+
+		ob_start();
+		if ( ! empty( $_REQUEST['no_placeholder'] ) )
+			$this->display_rows();
+		else
+			$this->display_rows_or_placeholder();
+		$rows = ob_get_clean();
+		
+		
+		ob_start();
+		$this->print_column_headers();
+		$headers = ob_get_clean();
+
+		ob_start();
+		$this->pagination('top');
+		$pagination_top = ob_get_clean();
+
+		ob_start();
+		$this->pagination('bottom');
+		$pagination_bottom = ob_get_clean();
+		
+		$response = array( 'rows' => $rows );
+		$response['pagination']['top'] = $pagination_top;
+		$response['pagination']['bottom'] = $pagination_bottom;
+		$response['column_headers'] = $headers;
+
+		if ( isset( $total_items ) )
+			$response['total_items_i18n'] = sprintf( _n( '1 item', '%s items', $total_items ), number_format_i18n( $total_items ) );
+
+		if ( isset( $total_pages ) ) {
+			$response['total_pages'] = $total_pages;
+			$response['total_pages_i18n'] = number_format_i18n( $total_pages );
+		}
+
+		//return $response;
+		wp_die( wp_json_encode($response) );
+	}
 	
 }
+
+
+/**
+ * Callback function for 'wp_ajax__ajax_fetch_custom_list' action hook.
+ * 
+ * Loads the Custom List Table Class and calls ajax_response method
+ */
+function update_skillset_list_table_ajax() {
+
+	$skill_list_table = new Skillset_List_Table();
+	$skill_list_table->ajax_response();
+}
+
+add_action('wp_ajax_update_skillset_list_table_ajax', 'update_skillset_list_table_ajax');
